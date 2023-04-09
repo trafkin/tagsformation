647410274648a010cfd6738deef2d4f8e8693e43
Christopher Valerio <christopher@valerio.guru>
2023-04-09 17:54:14 -0600

    Formating and adding stylish

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   description = "Flake for tagsformation";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
@@ -12,7 +14,8 @@
           tagsformation =
             final.haskell-nix.project' {
               src = ./.;
-              compiler-nix-name = "ghc927";
+              compiler-nix-name = "ghc925";
+              index-state = "2022-12-13T00:00:00Z";
               # This is used by `nix develop .` to open a shell for use with
               # `cabal`, `hlint` and `haskell-language-server`
               shell.tools = {
@@ -22,7 +25,7 @@
               };
               # Non-Haskell shell tools go here
               shell.buildInputs = with pkgs; [
-                nixpkgs-fmt
+                nixpkgs-fmt stylish-haskell
               ];
               # This adds `js-unknown-ghcjs-cabal` to the shell.
               # shell.crossPlatforms = p: [p.ghcjs];
@@ -39,4 +42,3 @@
       packages.default = flake.packages."tagsformation:exe:tagsformation";
     });
 }
-
